0312874b9b7d452c3dd1ece4068732421fe5b3b6
“Swapnapatil9” <swapnapatil629@gmail.com>
2023-03-09 10:22:54 +0530

    Added: Calculate Fare

---
+++ src/main/java/Invoice.java
@@ -0,0 +1,8 @@
+public class Invoice {
+    int totalRides;
+    double totalFare;
+    double avgFare;
+    public Invoice(double totalFare,int totalRides,double avgFare){
+
+    }
+}
---
+++ src/main/java/Ride.java
@@ -0,0 +1,8 @@
+public class Ride {
+    public double distance;
+    public double time;
+
+    public Ride(double distance, double time) {
+
+    }
+}
